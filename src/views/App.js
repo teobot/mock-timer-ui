59c130bc32f374c89d7d865f1cb7fc83206de457
theo.clapperton <theoclapperton@outlook.com>
2022-09-04 16:54:55 +0100

    updated

--- src/views/App.js
+++ src/views/App.js
@@ -8,14 +8,24 @@ import "../styles/App.css";
 
 function App() {
   const countDownRef = useRef();
+  const [countdownDate, setCountdownDate] = useState(Date.now() + 5000);
+  const [countdownDateHalfway, setCountdownDateHalfway] = useState({
+    minutes: 0,
+    seconds: 0,
+  });
+  const [userInputMinutes, setUserInputMinutes] = useState(0);
+  const [userInputSeconds, setUserInputSeconds] = useState(0);
+  const [startedCountdown, setStartedCountdown] = useState(false);
+
+  const [halfwayMessage, setHalfwayMessage] = useState(true);
+  const [lastMinuteMessage, setLastMinuteMessage] = useState(true);
+
   const [options, setOptions] = useState({
-    time: 0,
     prevTime: 0,
     volume: 1,
     rate: 1,
     pitch: 1,
-    started: false,
-    useSeconds: true,
+    useSeconds: false,
     onHalfwayMessage: "You are now halfway through your time for this section.",
     onLastMinuteMessage: "You have 1 minute remaining",
     onEndMessage: "Stop working, pencils down, your time is up.",
@@ -38,45 +48,133 @@ function App() {
     speechSynthesis.getVoices();
   }, []);
 
-  const renderer = ({ hours, minutes, seconds, completed }) => {
-    if (!options.started) {
-      return null;
+  const onTick = ({
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    completed,
+  }) => {
+    if (lastMinuteMessage) {
+      if (minutes === 1 && seconds === 0) {
+        textToSpeech(options.onLastMinuteMessage, () => {});
+      }
     }
+
+    if (
+      countdownDateHalfway.minutes === minutes &&
+      countdownDateHalfway.seconds === seconds
+    ) {
+      if (halfwayMessage) {
+        textToSpeech(options.onHalfwayMessage, () => {});
+      }
+    }
+  };
+
+  const renderer = ({
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    completed,
+  }) => {
+    if (!startedCountdown) return null;
+
     if (completed) {
       // Render a completed state
       return (
-        <div>
-          <h1>Time is up</h1>
+        <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+          <span
+            style={{
+              fontSize: "5rem",
+              fontWeight: "bold",
+            }}
+          >
+            Time is up
+          </span>
+          <a
+            style={{
+              fontSize: "2rem",
+              fontWeight: "bolder",
+            }}
+            href="window.location.reload()"
+          >
+            start again
+          </a>
         </div>
       );
     } else {
       // Render a countdown
       return (
-        <span>
-          {hours}:{minutes}:{seconds}
-        </span>
+        <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+          <div className="d-flex flex-row h-50 w-75 p-4">
+            <div className="w-50 h-100 me-2">
+              <Form.Control
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: "10vw",
+                  color: "#212529",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+                type="number"
+                value={minutes}
+                disabled
+              />
+              <p className="text-center lead">Minutes</p>
+            </div>
+
+            <div className="w-50 h-100 ms-2">
+              <Form.Control
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: "10vw",
+                  color: "#212529",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+                type="number"
+                value={seconds}
+                disabled
+              />
+
+              <p className="text-center lead">Seconds</p>
+            </div>
+          </div>
+        </div>
       );
     }
   };
 
-  const handlePlay = () => {
+  const handleStart = () => {
+    let minutes = userInputMinutes * 60 * 1000;
+    let seconds = userInputSeconds * 1000;
     textToSpeech(
-      `You have ${options.time} minutes for this section and your time starts now`,
+      `You have ${userInputMinutes > 0 ? `${userInputMinutes} minutes` : ""} ${
+        userInputSeconds > 0 ? `${userInputSeconds} seconds` : ""
+      } for this section and your time starts now`,
       () => {
-        setOptions({ ...options, started: true });
+        setStartedCountdown(true);
+        setCountdownDateHalfway({
+          minutes: Math.floor((minutes + seconds) / 2 / 1000 / 60),
+          seconds: Math.floor((minutes + seconds) / 2 / 1000) % 60,
+        });
+        setCountdownDate(Date.now() + minutes + seconds);
         countDownRef.current.start();
       }
     );
   };
 
-  const handlePause = () => {
-    countDownRef.current.pause();
-  };
-
   const handleComplete = () => {
-    textToSpeech(options.onEndMessage, () => {
-      setOptions({ ...options, started: false });
-    });
+    textToSpeech(options.onEndMessage, () => {});
   };
 
   return (
@@ -84,50 +182,162 @@ function App() {
       style={{
         height: "100%",
         width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        position: "relative",
       }}
     >
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link>Last Updated: {new Date().toLocaleTimeString()}</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Container>
-        <div className="m-2 p-2">
-          <Countdown
-            ref={countDownRef}
-            onComplete={handleComplete}
-            date={
-              Date.now() + options.time * 1000 * (options.useSeconds ? 1 : 60)
-            }
-            renderer={renderer}
-          />
-        </div>
-        <div className="p-2 m-2">
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Time (minutes)</Form.Label>
-            <Form.Control
-              type="number"
-              value={options.time}
-              onChange={(e) => {
-                setOptions({ ...options, time: e.target.value });
-              }}
-            />
-          </Form.Group>
-        </div>
-        <div className="p-2 m-2">
-          {options.started ? (
-            <Button variant="danger" onClick={handlePause}>
-              Pause
-            </Button>
+      <div className="w-100">
+        <Navbar bg="dark" variant="dark" className="position-relative">
+          <Container fluid>
+            <Nav className="me-auto">
+              <Nav.Link>
+                Last Updated: {new Date().toLocaleTimeString()}
+              </Nav.Link>
+              {/* RESET PAGE BUTTON */}
+              <Nav.Link
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                Reset
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="d-flex h-100 flex-column">
+        <Countdown
+          ref={countDownRef}
+          onComplete={handleComplete}
+          date={countdownDate}
+          renderer={renderer}
+          autoStart={false}
+          onTick={onTick}
+        />
+
+        {
+          // START COUNTDOWN
+          startedCountdown ? (
+            <>
+              <div className="w-100 h-25 p-1 mt-2 mb-2 d-flex justify-content-evenly">
+                <Button
+                  style={{
+                    fontSize: "2vw",
+                    fontWeight: "bold",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  className="w-25 h-100"
+                  variant="success"
+                  onClick={() => {
+                    countDownRef.current.pause();
+                  }}
+                >
+                  pause
+                </Button>
+                <Button
+                  style={{
+                    fontSize: "2vw",
+                    fontWeight: "bold",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  className="w-25 h-100"
+                  variant="primary"
+                  onClick={() => {
+                    countDownRef.current.start();
+                  }}
+                >
+                  resume
+                </Button>
+              </div>
+            </>
           ) : (
-            <Button variant="success" onClick={handlePlay}>
-              Play
-            </Button>
-          )}
-        </div>
-      </Container>
+            <div className="h-100 w-100 m-0 p-5 d-flex align-items-center justify-content-center">
+              <div className="w-75 h-100">
+                <div className="w-100 h-75 d-flex flex-row justify-content-center">
+                  <div className="w-50 h-100 pe-1">
+                    <Form.Control
+                      className="w-100 h-100"
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "10vw",
+                        color: "#212529",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                      }}
+                      max={60}
+                      min={0}
+                      type="number"
+                      value={userInputMinutes}
+                      onChange={(e) => {
+                        if (e.target.value > 60) {
+                          setUserInputMinutes(60);
+                        } else if (e.target.value < 0) {
+                          setUserInputMinutes(0);
+                        } else {
+                          setUserInputMinutes(e.target.value);
+                        }
+                      }}
+                    />
+                    <p className="text-center lead">Minutes</p>
+                  </div>
+                  <div className="w-50 h-100 ps-1">
+                    <Form.Control
+                      className="w-100 h-100"
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "10vw",
+                        color: "#212529",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                      }}
+                      max={60}
+                      min={0}
+                      type="number"
+                      value={userInputSeconds}
+                      onChange={(e) => {
+                        if (e.target.value > 60) {
+                          setUserInputSeconds(60);
+                        } else if (e.target.value < 0) {
+                          setUserInputSeconds(0);
+                        } else {
+                          setUserInputSeconds(e.target.value);
+                        }
+                      }}
+                    />
+                    <p className="text-center lead">Seconds</p>
+                  </div>
+                </div>
+                <div className="w-100 h-25 pt-5">
+                  <Button
+                    style={{
+                      fontSize: "3vw",
+                      fontWeight: "bold",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    className="w-100 h-100"
+                    variant="success"
+                    onClick={handleStart}
+                  >
+                    Play
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
